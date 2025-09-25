23bc099d1215e631426079f89ea9b9504b7a7ebc
HugoZAuad <hugozeymer@hotmail.com>
2025-09-25 15:47:37 -0300

    Fix: Ajuste na coluna

--- src/components/Components_Dashboard/SkillRow/SkillRow.tsx
+++ src/components/Components_Dashboard/SkillRow/SkillRow.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { TableRow, TableCell, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import type { Skill } from '../../../services/skillService/skillsService.types';
+import type { Skill, SkillCategory } from '../../../services/skillService/skillsService.types';
 
 interface Props {
   skill: Skill;
@@ -11,13 +11,23 @@ interface Props {
 }
 
 const formatLevel = (level: string) => {
-    return level.charAt(0) + level.slice(1).toLowerCase();
+  if (level === 'AVANCADO') return 'Avançado';
+  if (level === 'BASICO') return 'Básico';
+  if (level === 'EXPERIENTE') return 'Especialista';
+  return level.charAt(0) + level.slice(1).toLowerCase();
+};
+
+const formatCategory = (category: SkillCategory): string => {
+    return category.split('_').map(word => 
+        word.charAt(0) + word.slice(1).toLowerCase()
+    ).join(' ');
 };
 
 const SkillRow: React.FC<Props> = ({ skill, onEdit, onDelete }) => (
   <TableRow>
     <TableCell>{skill.name}</TableCell>
-    <TableCell>{formatLevel(skill.level)}</TableCell>
+    <TableCell>{formatCategory(skill.category)}</TableCell> 
+    <TableCell>{formatLevel(skill.level)}</TableCell>  
     <TableCell align="center" sx={{ width: '100px' }}>
       <IconButton onClick={onEdit}>
         <EditIcon />
